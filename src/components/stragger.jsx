6c698e5a76baee565d72e4168bbeb90e187c1482
zayennn <zaayeennn@gmail.com>
2025-11-27 04:37:52 +0700

    update

--- src/components/stragger.jsx
+++ src/components/stragger.jsx
@@ -1,33 +1,53 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { h3, span } from 'framer-motion/client'
 
 export const stragger = () => {
 
-    const parents = {
+    const parent = {
         show: {
             transition: {
-                straggerChildren: 0.2
+                staggerChildren: 0.1
             }
         }
     }
 
     const child = {
-        hidden: { opacity: 0 , y: 50 },
+        hidden: { opacity: 0, y: 10 },
         show: { opacity: 1, y: 0 }
     }
 
+    let name = "Elang Atha Zahran"
+    let arrOfName = [...name]
+
     return (
-        <motion.ul
-            variants={parents}
-            initial="hidden"
-            animate="show"
-        >
-            {[1, 2, 3].map(i => (
-                <motion.li key={i} variants={child}>
-                    item {i}
-                </motion.li>
-            ))}
-        </motion.ul>
+        <>
+            <h3>halo nama saya, </h3>
+            <motion.ul
+                variants={parent}
+                initial="hidden"
+                animate="show"
+                style={{ display: 'flex', padding: 0 }}
+            >
+                {arrOfName.map((char, i) => {
+                    if (char === " ") {
+                        return (
+                            <span key={i} style={{ width: "5px" }}></span>
+                        )
+                    }
+
+                    return (
+                        <motion.h1
+                            key={i}
+                            variants={child}
+                            style={{ margin: 0 }}
+                        >
+                            {char}
+                        </motion.h1>
+                    )
+                })}
+            </motion.ul>
+        </>
     )
 }
 
